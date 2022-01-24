fab3189861c1a136d5a3348dfff455c0265ee2b1
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2022-01-24 16:16:23 +0900

    muiテーマの反映

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '/styles/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Next.js handson</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
