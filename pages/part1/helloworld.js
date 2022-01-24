ee6507ac7447e3b84adc998f23ba31e26ed62d41
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2022-01-24 12:49:36 +0900

    Hello worldページ作成

---
+++ pages/part1/helloworld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <div>Welcome to Next.js!</div>
+    </div>
+  );
+};
+
+export default HelloWorld;
