fc8da79d8e7a81db8a00420f3d02f702d9535bfc
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2022-01-26 16:55:02 +0900

    APIリクエスト時に使う環境変数を設定

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const { VERCEL_URL, HOTPEPPER_API_KEY } = process.env;
+
 module.exports = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    VERCEL_URL,
+  },
+  serverRuntimeConfig: {
+    HOTPEPPER_API_KEY,
+  },
+};
