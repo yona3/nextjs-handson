52d44c8ace1f129183f01dd382a86b63a7c0a079
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2022-01-26 17:01:21 +0900

    MUIでリストデザイン整えた

--- pages/part1/gourmets.js
+++ pages/part1/gourmets.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import getConfig from 'next/config';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
 const fetchData = async (keyword) => {
   const { VERCEL_URL } = getConfig().publicRuntimeConfig;
@@ -13,11 +22,47 @@ const fetchData = async (keyword) => {
 
 const Shops = ({ shops }) => {
   return (
-    <ul>
-      {shops.map((shop) => {
-        return <li key={shop.id}>{shop.name}</li>;
-      })}
-    </ul>
+    <Container component="main" maxWidth="md">
+      <Box
+        component="form"
+        noValidate
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <List>
+          {shops.map((shop) => {
+            return (
+              <ListItem key={shop.id}>
+                <ListItemButton
+                  onClick={() => {
+                    // TODO: goto shop detail
+                  }}
+                >
+                  <ListItemAvatar>
+                    <Avatar alt={shop.name} src={shop.logo_image} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`${shop.genre.name} ${shop.name}`}
+                    secondary={
+                      <>
+                        <Typography variant="body1" component="span">
+                          {`${shop.catch} ${shop.shop_detail_memo}`}
+                        </Typography>
+                        <Typography variant="caption">{shop.address}</Typography>
+                      </>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Container>
   );
 };
 
