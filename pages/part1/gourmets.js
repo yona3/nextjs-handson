76d49a7fe2436fbd9645c4d824baed5fa039666a
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2022-01-27 14:16:49 +0900

    フォーム入力し検索ボタンを押した時にCSRでデータ取得する

--- pages/part1/gourmets.js
+++ pages/part1/gourmets.js
@@ -31,6 +31,13 @@ const Shops = ({ firstViewShops }) => {
     setShops(firstViewShops);
   }, [firstViewShops]);
 
+  const onSearchClick = async () => {
+    const data = await fetchData(keyword);
+
+    setShops(data);
+    setKeyword('');
+  };
+
   return (
     <Container component="main" maxWidth="md">
       <Box
@@ -59,7 +66,7 @@ const Shops = ({ firstViewShops }) => {
           margin="normal"
           fullWidth
           onClick={() => {
-            setKeyword('');
+            onSearchClick();
           }}
         >
           検索
