c7f67c0a48446cc6861cb14043422f93e87945a4
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2022-02-04 10:23:13 +0900

    ジャンル更新API作成 PATCH /api/genres/:id

--- pages/api/genres/[id].js
+++ pages/api/genres/[id].js
@@ -12,10 +12,31 @@ const getGenre = async (id) => {
   return genre;
 };
 
+const updateGenre = async (id, { code, name }) => {
+  const prisma = new PrismaClient();
+
+  const data = {};
+  if (code) data.code = code;
+  if (name) data.name = name;
+
+  const genre = await prisma.genres.update({
+    where: {
+      id,
+    },
+    data,
+  });
+
+  return genre;
+};
+
 const genre = async (req, res) => {
   switch (req.method) {
     case 'GET':
       return res.status(200).json(await getGenre(parseInt(req.query.id, 10)));
+      break;
+
+    case 'PATCH':
+      return res.status(200).json(await updateGenre(parseInt(req.query.id, 10), req.body));
       break;
 
     default:
