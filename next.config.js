9c51356695dc7a98588099fe5ac6d02a167020b6
arakawamoriyuki <arakawamoriyuki@gmail.com>
2022-02-07 11:16:37 +0900

    revert part1 setup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const { VERCEL_URL, HOTPEPPER_API_KEY } = process.env;
-
 module.exports = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    API_HOST: VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000',
-  },
-  serverRuntimeConfig: {
-    HOTPEPPER_API_KEY,
-  },
-};
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
-import App from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '/styles/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Next.js handson</title>
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
