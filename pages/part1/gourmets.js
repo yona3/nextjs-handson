f6b4e4f6191ae79b7d4bae820331f1b5db1a8149
yona3 <yonajs208@gmail.com>
2024-02-26 18:07:08 +0900

    feature search

--- pages/part1/gourmets.js
+++ pages/part1/gourmets.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import getConfig from 'next/config';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
 const fetchData = async (keyword) => {
   const { API_HOST } = getConfig().publicRuntimeConfig;
@@ -12,13 +23,95 @@ const fetchData = async (keyword) => {
   return await res.json();
 };
 
-const Shops = ({ shops }) => {
+const Shops = ({ firstViewShops }) => {
+  const [keyword, setKeyword] = React.useState('');
+  const [shops, setShops] = React.useState([]);
+
+  useEffect(() => {
+    setShops(firstViewShops);
+  }, [firstViewShops]);
+
+  const onSearchClick = async () => {
+    const data = await fetchData(keyword);
+
+    setShops(data);
+    setKeyword('');
+  };
+
   return (
-    <ul>
-      {shops.map((shop) => {
-        return <li key={shop.id}>{shop.name}</li>;
-      })}
-    </ul>
+    <Container component="main" maxWidth="md">
+      <Box
+        component="form"
+        noValidate
+        maxWidth="md"
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <TextField
+          label="キーワードを入力してください"
+          variant="standard"
+          margin="normal"
+          fullWidth
+          value={keyword}
+          onChange={(event) => {
+            setKeyword(event.target.value);
+          }}
+        />
+        <Button
+          variant="contained"
+          margin="normal"
+          fullWidth
+          onClick={() => {
+            onSearchClick();
+          }}
+        >
+          検索
+        </Button>
+      </Box>
+      <Box
+        component="form"
+        noValidate
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <List>
+          {shops.map((shop) => {
+            return (
+              <ListItem key={shop.id}>
+                <ListItemButton
+                  onClick={() => {
+                    // TODO: goto shop detail
+                  }}
+                >
+                  <ListItemAvatar>
+                    <Avatar alt={shop.name} src={shop.logo_image} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`${shop.genre.name} ${shop.name}`}
+                    secondary={
+                      <>
+                        <Typography variant="body1" component="span">
+                          {`${shop.catch} ${shop.shop_detail_memo}`}
+                        </Typography>
+                        <Typography variant="caption">{shop.address}</Typography>
+                      </>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Container>
   );
 };
 
@@ -27,7 +120,7 @@ export const getServerSideProps = async (req) => {
 
   return {
     props: {
-      shops: data,
+      firstViewShops: data,
     },
   };
 };
